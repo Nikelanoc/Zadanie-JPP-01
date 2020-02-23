66af3d265a2f86c3407c86165cf1e38f7f104aa3
Nikola <nikola.nocon@gmail.com>
2020-02-23 11:24:02 +0100

    Added min and max

--- src/Z1.java
+++ src/Z1.java
@@ -5,21 +5,24 @@ public class Z1 {
         int min = findMinimum(tab);
         System.out.println("Max: " + max + "Min: " + min);
 
-    }private static int findMaximum(int[] tab) {
+    }
+
+    private static int findMaximum(int[] tab) {
         int potentialMax = tab[0];
         for (int i = 1; i < tab.length; i++) {
             if (tab[i] > potentialMax) {
                 potentialMax = tab[i];
             }
-            return potentialMax;
-        }
+        }return potentialMax;
+
     }
+
     private static int findMinimum(int[] tab) {
         int potentialMin = tab[0];
         for (int i = 1; i < tab.length; i++) {
             if (tab[i] < potentialMin) {
                 potentialMin = tab[i];
             }
-            return potentialMin;
-        }
-    }}
+        } return potentialMin;
+    }
+}

2493b3e729fc643eb93285d20683843b3b73c887
Nikola <nikola.nocon@gmail.com>
2020-02-23 10:25:40 +0100

    Added branch

--- src/Z1.java
+++ src/Z1.java
@@ -1,5 +1,10 @@
 public class Z1 {
     public static void main(String[] args) {
+        int [] tab = new int[10];
+        for (int i = 0; i <tab.length; i++) {
+            tab[i] = i;
 
+        }
+        System.out.println();
     }
 }
